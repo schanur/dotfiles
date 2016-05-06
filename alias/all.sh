d46439f6f663a9c78fbb139f50ad89cca2fa6b75
schanur <b.griebenow@web.de>
2016-05-06 09:32:24 +0200

    aliases for mplayer and loader for all alias modules

---
+++ alias/all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_FILENAME=$(readlink -f $0)
+SCRIPT_PATH=$(dirname $SCRIPT_FILENAME)
+INCLUDE_PATH=${SCRIPT_PATH}/shared_bash
+
+source ${INCLUDE_PATH}/path_variable.sh
+
+for INC_FILE in .; do
+    echo "Load alial file: ${INC_FILE}"
+    . ${INC_FILE}
+done
