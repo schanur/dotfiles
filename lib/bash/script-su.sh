5bcee710812acbeadab023db3d893215dbaf100e
schanur <b.griebenow@web.de>
2016-07-05 21:30:44 +0200

    Print short message when requesting root password

--- lib/bash/script-su.sh
+++ lib/bash/script-su.sh
@@ -1,6 +1,8 @@
 
 function run_as_root {
     if [ "${USER}" != "root" ]; then
+        echo "Root privileges are required.
+Please enter root password to continue or CTRL+C to abort."
         su -c "${0}" root
         exit
     fi
