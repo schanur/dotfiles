37a1a236d5c20074612e25a16ac78d4120750d50
Bjoern Griebenow <b.griebenow@web.de>
2016-09-15 22:41:35 +0200

    Rename install-debs to debian-package-group and allow the installation/removal of single package groups

--- lib/bash/script-su.sh
+++ lib/bash/script-su.sh
@@ -3,7 +3,7 @@ function run_as_root {
     if [ "${USER}" != "root" ]; then
         echo "Root privileges are required.
 Please enter root password to continue or CTRL+C to abort."
-        su -c "${0} ${*}" root
+        su root -c "echo && ${0} ${*}"
         exit
     fi
 }
