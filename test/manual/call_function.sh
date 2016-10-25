0f6c2be1a504faa9317c62b57ceb54e9eee84d7e
Bjoern Griebenow <b.griebenow@web.de>
2016-10-25 09:52:19 +0200

    Add unfinished manual integration test

---
+++ test/manual/call_function.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+SCRIPT_FILENAME=$(readlink -f $0)
+SCRIPT_PATH=$(dirname $SCRIPT_FILENAME)
+DOTFILES_PATH=${SCRIPT_PATH}/../..
+
+source ${DOTFILES_PATH}/lib/bash/debug.sh
+
+
+function test_local_vars {
+    local TEST_1=0
+    local TEST_2=1
+
+    stack_trace
+
+}
+
+test_local_vars
