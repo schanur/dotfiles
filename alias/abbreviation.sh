c45d80e6982268e25b041af11018d7e6512e2745
Bjoern Griebenow <b.griebenow@web.de>
2016-11-06 07:39:20 +0100

    Add alias file with abbreviations

---
+++ alias/abbreviation.sh
@@ -0,0 +1,9 @@
+
+# Software management.
+alias sma='su  -c "apt-get update && aptitude upgrade"'
+alias smud='su -c "apt-get update"'
+alias smug='su -c "aptitude upgrade"'
+
+alias ll='ls -la'
+# Show hidden files only.
+alias l.='ls -d .* --color=auto'
