e646b4246f382cd464e4583707cd45d39ed5653c
Bjoern Griebenow <b.griebenow@web.de>
2017-01-25 22:33:23 +0100

    Move bash library functions to a separate repository

--- lib/bash/args.sh
+++
@@ -1,7 +0,0 @@
-
-source ${DOTFILES_PATH}/lib/bash/error.sh
-
-
-function flag_option_set {
-    not_implemented_error
-}
--- lib/bash/date.sh
+++
@@ -1,5 +0,0 @@
-
-# Prints a number only timestamp in the format YYYYMMDDhhmmss.
-function timestamp {
-    date --rfc-3339=seconds |sed -e 's/[-\ :]//g' |sed -e 's/\+.*$//g'
-}
