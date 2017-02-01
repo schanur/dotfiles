601f0d066c907ee6688f3dee4094b7e46128113b
Bjoern Griebenow <b.griebenow@web.de>
2017-02-01 07:55:25 +0100

    Do not include path_variable.sh module in alias files

--- alias/all.sh
+++ alias/all.sh
@@ -4,9 +4,9 @@ SCRIPT_FILENAME=$(readlink -f $0)
 SCRIPT_PATH=$(dirname $SCRIPT_FILENAME)
 DOTFILES_PATH=${SCRIPT_PATH}/..
 
-source ${DOTFILES_PATH}/lib/bash/path_variable.sh
-
 echo "Load alias files"
+echo
+
 for INC_FILE in $(find ${DOTFILES_PATH}/alias |grep -v "all.sh"); do
     echo "Load alias file: ${INC_FILE}"
     . ${INC_FILE}
