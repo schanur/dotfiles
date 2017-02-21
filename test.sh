dc65536af3504e6f535a2a90758ad0e8a26e46f3
Bjoern Griebenow <b.griebenow@web.de>
2017-02-21 07:09:20 +0100

    Add global test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+SCRIPT_FILENAME=$(readlink -f $0)
+SCRIPT_PATH=$(dirname $SCRIPT_FILENAME)
+DOTFILES_PATH=${SCRIPT_PATH}
+
+vendor/libbivalvia/test.sh
+# ${DOTFILES_PATH}/test/scripts-have-short-description.sh
