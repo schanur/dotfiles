697cdd065085efc349bba2d9b863e5b13a1782b9
Bjoern Griebenow <b.griebenow@web.de>
2017-04-18 09:19:00 +0200

    Only process alias files with ".sh" prefix

--- alias/all.sh
+++ alias/all.sh
@@ -4,7 +4,7 @@
 # DOTFILES_PATH=${SCRIPT_PATH}/..
 
 
-for INC_FILE in $(find ${DOTFILES_PATH}/alias |grep -v "all.sh"); do
+for INC_FILE in $(find ${DOTFILES_PATH}/alias -name "*.sh" |grep -v "all.sh"); do
     # echo "Load alias file: ${INC_FILE}"
     if [ ! -d ${INC_FILE} ]; then
         echo -n " $(basename -s .sh ${INC_FILE})"
