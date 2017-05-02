733350da5f42beff0bbe8c18634e126467653a98
Bjoern Griebenow <b.griebenow@web.de>
2017-05-02 09:26:53 +0200

    Add alias to grep through all versions of the repository

--- alias/git.sh
+++ alias/git.sh
@@ -8,5 +8,8 @@ alias gs-sync_dirty='git stash && gs-pull && git svn dcommit && git stash pop'
 # Remove all remotes
 alias git-remove-all-remotes='for GIT_REMOTE in $(git remote); do git remote remove ${GIT_REMOTE}; done'
 
+# Dangerous!
 alias git-brutal-credential-fix='git-change-credentials -a b.griebenow@web.de "Bjoern Griebenow" && rm -rfv .git/refs/original/refs/heads/master && git push -u github --all  --force'
 alias gbcf='git-brutal-credential-fix'
+
+alias git-search-history='git rev-list --all | xargs git grep'
