1c13c21f59c324a924b3fb20448be838e3d89e98
Bjoern Griebenow <b.griebenow@web.de>
2017-06-02 17:01:09 +0200

    Normalize git alias names

--- alias/git.sh
+++ alias/git.sh
@@ -2,8 +2,8 @@
 # git SVN
 alias gs-pull='git svn fetch   && git svn rebase'
 alias gs-sync='gs-pull         && git svn dcommit'
-alias gs-pull_dirty='git stash && gs-pull &&                    git stash pop'
-alias gs-sync_dirty='git stash && gs-pull && git svn dcommit && git stash pop'
+alias gs-pull-dirty='git stash && gs-pull &&                    git stash pop'
+alias gs-sync-dirty='git stash && gs-pull && git svn dcommit && git stash pop'
 
 # Remove all remotes
 alias git-remove-all-remotes='for GIT_REMOTE in $(git remote); do git remote remove ${GIT_REMOTE}; done'
