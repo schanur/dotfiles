da98ed7e16ade48dda0b135789185d4ac06d1cc7
Bjoern Griebenow <b.griebenow@web.de>
2018-01-01 12:12:47 +0100

    Add 2 regression tests

--- test.sh
+++ test.sh
@@ -5,5 +5,13 @@ SCRIPT_FILENAME="$(readlink -f "${0}")"
 SCRIPT_PATH="$(dirname "${SCRIPT_FILENAME}")"
 DOTFILES_PATH="${SCRIPT_PATH}"
 
-vendor/libbivalvia/test.sh
-# ${DOTFILES_PATH}/test/scripts-have-short-description.sh
+
+# Check if submodules pass tests.
+# vendor/libbivalvia/test.sh
+
+
+# Run all regression tests.
+for TEST_SCRIPT in $(find "${DOTFILES_PATH}/test/regression" -type f); do
+    echo "${TEST_SCRIPT}"
+    "${TEST_SCRIPT}"
+done
