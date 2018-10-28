d71b083f3ded6eb28ca28676835283b33e3341fd
Bjoern Griebenow <b.griebenow@web.de>
2018-10-28 11:05:42 +0100

    Split factor_long_time into 3 aliases with different CPU runtime

--- alias/misc.sh
+++ alias/misc.sh
@@ -1,1 +1,4 @@
-alias factor_long_time='factor 12121212121212121212121212121212121212'
+# Test time measurement function from aftermath plugin.
+alias factor-short-time='factor 123123123123123123123123123123123123117'  # Needs approx 1-2 seconds
+alias factor-medium-time='factor 123123123123123123123123123123123123121' # Needs approx 20 seconds
+alias factor-long-time='factor 12121212121212121212121212121212121212'    # Needs approx 2 minutes
