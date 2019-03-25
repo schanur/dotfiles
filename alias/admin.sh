6903dc9c27994b47d0259eb2b97fcda50804c949
Bjoern Griebenow <b.griebenow@web.de>
2019-03-25 16:01:48 +0100

    Add alias for kernel function profiling

--- alias/admin.sh
+++ alias/admin.sh
@@ -1,2 +1,5 @@
 # 'du space' Show first 30 folders that use most space.
 alias dus='du -h --max-depth=1 2>/dev/null | sort -r -h | head -n 30'
+
+# List kernel functions with most CPU time spend.
+perf record -g -a sleep 3 && perf report
