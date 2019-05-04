d88dc41d6eeb5dbd3c49fcda3ae677486402ea9b
Bjoern Griebenow <b.griebenow@web.de>
2019-05-04 13:52:44 +0200

    Move xroot background color init code to x11session.sh
    Does not work anyway.

--- x11/x11session.sh
+++ x11/x11session.sh
@@ -6,6 +6,10 @@ redshift &
 # IBM Model M does not have a Super(Win) key. Use CAPS Lock instead.
 setxkbmap -option caps:super
 
+# Set background color.
+xsetroot -solid darkred
+
+
 echo "$(date -Iseconds): shared/x11session: $0" >> "${HOME}/user.log"
 
 export CONF_FILE_LOADED__X11SESSION=1
