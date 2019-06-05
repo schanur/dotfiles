e87603bb5116ff0ca7b6c731845558f6518801e3
Bjoern Griebenow <b.griebenow@web.de>
2019-06-05 23:23:56 +0200

    Add alias for sxiv to start in fullscreen mode and scale to fit

--- alias/mm.sh
+++ alias/mm.sh
@@ -1,3 +1,7 @@
 # Rip CD and encode to FLAC. Stop if CD does not match an entry in
 # Freedb database.
 alias rip-cd-flac='ripit -o . -c 2 -q 8 --nointeraction --quitnodb 1'
+
+# Start in fullscreen mode and with scale to fit.
+# sxiv still has no way to start with anti aliasing disabled.
+alias sxiv='sxiv -f -s f'
