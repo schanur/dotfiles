1e900e76c14df67ce0925e22f9726cdd2e8e5e62
Bjoern Griebenow <b.griebenow@web.de>
2019-06-22 18:57:22 +0200

    Add alias to print a backtrace of a crashing application

--- alias/dev.sh
+++ alias/dev.sh
@@ -3,3 +3,6 @@ if [ "${SHELL_NAME}" = "zsh" ]; then
 else
     alias reload="source ~/.bashrc && xrdb ~/.Xresources"
 fi
+
+# Print backtrace of a crashing application.
+alias bt='gdb -batch -ex "run" -ex "bt" -ex "quit"'
