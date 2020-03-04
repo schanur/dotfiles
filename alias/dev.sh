3f73ec8d72bd65949ed6ed757f6ef9e4ab5bcb8b
Bjoern Griebenow <b.griebenow@web.de>
2020-03-04 08:43:54 +0100

    Add alias to execute all following commands with jemalloc preloaded

--- alias/dev.sh
+++ alias/dev.sh
@@ -7,4 +7,9 @@ fi
 # Print backtrace of a crashing application.
 alias bt='gdb -batch -ex "run" -ex "bt" -ex "quit"'
 
+# less, but with syntax highlighting.
 alias lessh='LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s" less -M '
+
+
+# Preload every application with jemalloc memory allocator.
+alias jemalloc-preload="export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libjemalloc.so"
