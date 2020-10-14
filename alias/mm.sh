212ecc0ffdf08b6d1326769a563c9ef0d4b4b0d8
Bjoern Griebenow <b.griebenow@web.de>
2020-10-14 22:07:59 +0200

    feat: add alias for recording mixed speaker/headset sound

--- alias/mm.sh
+++ alias/mm.sh
@@ -5,3 +5,8 @@ alias rip-cd-flac='ripit -o . -c 2 -q 8 --nointeraction --quitnodb 1'
 # Start in fullscreen mode and with scale to fit.
 # sxiv still has no way to start with anti aliasing disabled.
 alias sxiv='sxiv -f -s f'
+
+# Record the sound mix that goes to the speaker.
+# Call with WAV64 filename as parameter.
+# Example: soundcard_grep soundfile.w64
+alias soundcard_grep='parec -d alsa_output.pci-0000_00_1f.3.analog-stereo.monitor --file-format=w64'
