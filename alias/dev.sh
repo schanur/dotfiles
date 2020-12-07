c50d1b67d0771dd3240848b8cf7b6b16f16fd62a
Bjoern Griebenow <b.griebenow@web.de>
2020-12-07 01:36:45 +0100

    feat: bt alias also breaks on assert()

--- alias/dev.sh
+++ alias/dev.sh
@@ -5,7 +5,7 @@ else
 fi
 
 # Print backtrace of a crashing application.
-alias bt='gdb -batch -ex "run" -ex "bt" -ex "quit"'
+alias bt='gdb -batch -ex "break assert" -ex "run" -ex "bt" -ex "quit"'
 
 # less, but with syntax highlighting.
 alias lessh='LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s" less -M '
