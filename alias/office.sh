2e335a1e620607d89d0400ec3bc88b6dc5abe0e5
Bjoern Griebenow <b.griebenow@web.de>
2022-03-09 17:59:47 +0100

    feat: add alias for lower resolution paper scanning

--- alias/office.sh
+++ alias/office.sh
@@ -1,4 +1,4 @@
 # Usage: scan-a4 > DOC_NAME.png
 alias scan-a4='scanimage --format png -x 215 -y 297 --resolution 600'
 
-
+alias scan-a4-150dpi='scanimage --format png -x 215 -y 297 --resolution 150'
