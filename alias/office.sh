d66ded4c8e7751318c36c233b05b2b5670f67a6b
Bjoern Griebenow <b.griebenow@web.de>
2022-07-05 20:12:15 +0200

    feat: add more scan sizes and resolutions as shell aliases

--- alias/office.sh
+++ alias/office.sh
@@ -1,4 +1,10 @@
 # Usage: scan-a4 > DOC_NAME.png
-alias scan-a4='scanimage --format png -x 215 -y 297 --resolution 600'
-
+alias scan-a4='scanimage        --format png -x 215 -y 297 --resolution 600'
+alias scan-a4-150dpi='scanimage --format png -x 215 -y 297 --resolution 300'
 alias scan-a4-150dpi='scanimage --format png -x 215 -y 297 --resolution 150'
+alias scan-a4-75dpi='scanimage  --format png -x 215 -y 297 --resolution 75'
+
+alias scan-a5='scanimage        --format png -x 108 -y 149 --resolution 600'
+alias scan-a5-300dpi='scanimage --format png -x 108 -y 149 --resolution 300'
+alias scan-a5-150dpi='scanimage --format png -x 108 -y 149 --resolution 150'
+alias scan-a5-75dpi='scanimage  --format png -x 108 -y 149 --resolution 75'
