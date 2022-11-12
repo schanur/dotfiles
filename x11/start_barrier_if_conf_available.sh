d4474a7b02eabf3634461c88412808759530017f
Bjoern Griebenow <b.griebenow@web.de>
2022-11-12 22:39:23 +0100

    refactor: do not start barrier twice

--- x11/start_barrier_if_conf_available.sh
+++ x11/start_barrier_if_conf_available.sh
@@ -12,16 +12,31 @@ DOTFILES_PATH="${SCRIPT_PATH}/.."
 source "${DOTFILES_PATH}/lib/bivalvia/help.sh"
 
 
+function barrier_is_running {
+    local BARRIER_IS_RUNNING=$(ps aux | grep -ce 'barrier.*--disable-crypto --no-daemon' || true)
+    (( BARRIER_IS_RUNNING-- ))
+    echo ${BARRIER_IS_RUNNING}
+}
+
+
 function start_server {
-    barriers --debug INFO --log /tmp/barrier_server.log --name server --config "${DOTFILES_PATH}/barrier/server.conf"
-    # barrier --debug INFO --log /tmp/barrier_server.log --name server --config "${DOTFILES_PATH}/barrier/server.conf"
+    touch "/tmp/barrier_server.log"
+    barriers --disable-crypto --no-daemon --debug DEBUG --log /tmp/barrier_server.log --name server --address :: --config "${DOTFILES_PATH}/barrier/server.conf"
 }
 
 
 function start_client {
-    # barrierc --debug INFO --log /tmp/barrier_client.log --name client1 --config "${DOTFILES_PATH}/barrier/client.conf"
-    barrierc --debug INFO --log /tmp/barrier_client.log --name client1 "$BARRIER_SERVER"
+    touch "/tmp/barrier_client.log"
+    # Even activating IPv6 avahi did not macke "[]:24800" autoconfig work.
+    barrierc --disable-crypto --no-daemon --debug DEBUG --log /tmp/barrier_client.log --name client1 "[mizar.local]:24800"
 }
+
+
+if [ $(barrier_is_running) -eq 1 ]; then
+    echo "Barrier is already running. Do nothing."
+    exit 0
+fi
+
 
 if [ "$(cat /etc/machine-id | sha512sum | cut -f 1 -d ' ')" = "56314343caf6ab223f04b7261f6d8cc5732aaacffdf04a66d420069f9a77e4e6c3e231b45bea4338a190b477e552ffdc874e9b5b699d6c2258fb51848d3535f3" ]; then
     echo "Start barrier server."
